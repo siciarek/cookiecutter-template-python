7b6f5ef598a00802914a198d9704671648d4b270
Jacek Siciarek <siciarek@gmail.com>
2025-04-18 01:20:47 +0200

    feat: update dependencies

--- {{cookiecutter.project_slug}}/tests/lib/test_dummy.py
+++ {{cookiecutter.project_slug}}/tests/lib/test_dummy.py
@@ -1,6 +1,13 @@
+import pytest
 from {{cookiecutter.project_pkg_name}}.lib.dummy import Dummy
 
 
-def test_hello():
-    assert Dummy().say_hello() == "Hello, World!"
-    assert Dummy("Handsome").say_hello() == "Hello, Handsome!"
+hello_data_provider = (
+    (None, "Hello, World!", "No constructor args."),
+    ("Handsome", "Hello, Handsome!", "Custom name: Handsome."),
+)
+
+@pytest.mark.parametrize("name, expected, messsage", hello_data_provider)
+def test_hello(name, expected, message):
+    srv = Dummy(name=name) if name else Dummy()
+    assert srv.say_hello() == expected, message
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_pkg_name}}/entrypoints/main.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_pkg_name}}/entrypoints/main.py
@@ -7,6 +7,6 @@ from {{cookiecutter.project_pkg_name}}.lib.dummy import Dummy
 @click.option("-n", "--name", type=str, default="World")
 def main(name: str):
     Ioc.logger().debug("START")
-    message = Dummy().say_hello(name=name)
+    message = Dummy(name=name).say_hello()
     click.echo(click.style(message, fg="green", reverse=True))
     Ioc.logger().debug("END")
