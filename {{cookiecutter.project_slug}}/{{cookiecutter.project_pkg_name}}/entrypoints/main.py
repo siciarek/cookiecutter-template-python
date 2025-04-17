444a38d7d93f90737aa40bbbbde483571d0ce849
Jacek Siciarek <siciarek@gmail.com>
2025-04-18 00:58:13 +0200

    refactor: rename default entyrpoint

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_pkg_name}}/entrypoints/main.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_pkg_name}}/entrypoints/main.py
@@ -1,9 +1,12 @@
 import click
+from {{cookiecutter.project_pkg_name}}.container import Ioc
 from {{cookiecutter.project_pkg_name}}.lib.dummy import Dummy
 
 
 @click.command()
 @click.option("-n", "--name", type=str, default="World")
 def main(name: str):
+    Ioc.logger().debug("START")
     message = Dummy().say_hello(name=name)
-    click.echo(message=message)
+    click.echo(click.style(message, fg="green", reverse=True))
+    Ioc.logger().debug("END")
