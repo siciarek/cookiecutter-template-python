f3f835ddb7c3c1f7ebf9f614e92c668f61d42ea4
Jacek Siciarek <siciarek@gmail.com>
2025-04-18 15:58:23 +0200

    feat:Update tests

--- {{cookiecutter.project_slug}}/tests/lib/test_dummy.py
+++ {{cookiecutter.project_slug}}/tests/lib/test_dummy.py
@@ -5,6 +5,7 @@ from {{cookiecutter.project_pkg_name}}.lib.dummy import Dummy
 hello_data_provider = (
     (None, "Hello, World!", "No constructor args."),
     ("Handsome", "Hello, Handsome!", "Custom name: Handsome."),
+    ("Gorgeous", "Hello, Gorgeous!", "Custom name: Gorgeous."),
 )
 
 @pytest.mark.parametrize("name, expected, message", hello_data_provider)
